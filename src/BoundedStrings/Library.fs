dc6e72146052e1402b254f9d19d51189f0db253e
Christopher Atkins <christopher.atkins@relayfoods.com>
2016-03-03 23:07:40 -0500

    factory method for Option<FixedLengthString>

--- src/BoundedStrings/Library.fs
+++ src/BoundedStrings/Library.fs
@@ -29,9 +29,19 @@ type DependentTypesProvider (cfg) as tp =
         else
         s'
        @@>
-    let ctor = new ProvidedConstructor([param], InvokeCode = constrain)
-    let fixedStr = new ProvidedTypeDefinition(asm, ns, tyName, Some typeof<string>)
+    let ctor        = new ProvidedConstructor([param], InvokeCode = constrain)
+    let fixedStr    = new ProvidedTypeDefinition(asm, ns, tyName, Some typeof<string>)
+    let fixedStrOpt = typedefof<_ option>.MakeGenericType(fixedStr)
+    let factory     = new ProvidedMethod("TryCreate", [param], fixedStrOpt, IsStaticMethod = true)
+    factory.InvokeCode <-
+      fun (v :: []) ->
+          <@@
+          let s' = (%%Expr.Coerce(v, typeof<string>) : string)
+          if s'.Length > (int l) then None
+          else Some s' @@>
+
     fixedStr.AddMember(ctor)
+    fixedStr.AddMember(factory)
     fixedStr
 
   do
--- tests/BoundedStrings.Tests/Tests.fs
+++ tests/BoundedStrings.Tests/Tests.fs
@@ -17,3 +17,12 @@ let ``string should fit`` () =
 let ``string shouldn't fit`` () =
   Assert.Throws(fun () -> F("testing!!!!!!") |> ignore)
   |> Assert.IsNotNull
+
+[<Test>]
+let ``should be able to create option`` () =
+  let (Some f) = F.TryCreate("test")
+  Assert.IsNotNullOrEmpty(upcast f)
+
+[<Test>]
+let ``too long should be None`` () =
+  Assert.IsTrue(F.TryCreate("test!!!!!!").IsNone)
